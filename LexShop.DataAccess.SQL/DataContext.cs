d4b0777a2c908a1daa90f8619d54bc1c98210afd
Daryl <ng.daryl@gmail.com>
2021-01-12 19:39:53 +0100

    Added EntityFramework (Db Migration)

---
+++ LexShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using LexShop.Core.Models;
+
+namespace LexShop.DataAccess.SQL
+{
+    class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCatagories { get; set; }
+    }
+}
